d56c01bdd94cacbfbb3a4fb89c4cbed4963f494e
Lu Quan Tan <LQ@Lus-MacBook-Pro.local>
2014-11-29 15:24:56 -0500

    Initial commit with FlickrAPIKey added.

---
+++ FlickrPhotoFetch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrPhotoFetch
+//
+//  Created by Lu Quan Tan on 11/29/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrPhotoFetch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlickrPhotoFetch
+//
+//  Created by Lu Quan Tan on 11/29/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
