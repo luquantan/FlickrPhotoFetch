1011d58a903f05b57a9a0dd97178003ae9a3509f
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-12-04 17:32:46 -0500

    Initial commit of LQApplicationSettings

---
+++ LQApplicationSettings.h
@@ -0,0 +1,13 @@
+//
+//  LQApplicationSettings.h
+//  FlickrPhotoFetch
+//
+//  Created by LuQuan Intrepid on 12/4/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQApplicationSettings : NSObject
+
+@end
---
+++ LQApplicationSettings.m
@@ -0,0 +1,13 @@
+//
+//  LQApplicationSettings.m
+//  FlickrPhotoFetch
+//
+//  Created by LuQuan Intrepid on 12/4/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import "LQApplicationSettings.h"
+
+@implementation LQApplicationSettings
+
+@end
