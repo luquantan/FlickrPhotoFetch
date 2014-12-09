12f9b917cc3c0f01acbd4f294d755c66a14f2e5e
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-12-08 19:01:48 -0500

    Added TopPlacesSplitVC.

---
+++ FlickrPhotoFetch/TopPlacesSpiltVC.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesSpiltVC.h
+//  FlickrPhotoFetch
+//
+//  Created by LuQuan Intrepid on 12/8/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesSpiltVC : UISplitViewController
+
+@end
---
+++ FlickrPhotoFetch/TopPlacesSpiltVC.m
@@ -0,0 +1,46 @@
+//
+//  TopPlacesSpiltVC.m
+//  FlickrPhotoFetch
+//
+//  Created by LuQuan Intrepid on 12/8/14.
+//  Copyright (c) 2014 LuQuanTan. All rights reserved.
+//
+
+#import "TopPlacesSpiltVC.h"
+
+@interface TopPlacesSpiltVC () <UISplitViewControllerDelegate>
+
+@end
+
+@implementation TopPlacesSpiltVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)awakeFromNib
+{
+    
+}
+
+- (void)splitViewController:(UISplitViewController *)svc willChangeToDisplayMode:(UISplitViewControllerDisplayMode)displayMode
+{
+    
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
